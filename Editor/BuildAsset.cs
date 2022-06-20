2a77e0dc27558241e50dc7a979c1f96a855a2551
wellshsu <wellshsu@outlook.com>
2022-06-20 11:02:13 +0800

    initial commit

---
+++ Editor/BuildAsset.cs
@@ -0,0 +1,187 @@
+﻿//---------------------------------------------------------------------//
+//                    GNU GENERAL PUBLIC LICENSE                       //
+//                       Version 2, June 1991                          //
+//                                                                     //
+// Copyright (C) Wells Hsu, wellshsu@outlook.com, All rights reserved. //
+// Everyone is permitted to copy and distribute verbatim copies        //
+// of this license document, but changing it is not allowed.           //
+//                  SEE LICENSE.md FOR MORE DETAILS.                   //
+//---------------------------------------------------------------------//
+using System;
+using System.IO;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using EP.U3D.EDITOR.BASE;
+using EP.U3D.LIBRARY.ASSET;
+
+namespace EP.U3D.EDITOR.ASSET
+{
+    public class BuildAsset
+    {
+        public static Type WorkerType = typeof(BuildAsset);
+
+        [MenuItem(Constants.MENU_PATCH_BUILD_ASSET)]
+        public static void Invoke1()
+        {
+            var worker = Activator.CreateInstance(WorkerType) as BuildAsset;
+            worker.BuildAll();
+        }
+
+        [MenuItem(Constants.MENU_PATCH_BUILD_SCENE)]
+        public static void Invoke2()
+        {
+            Helper.CollectScenes();
+        }
+
+        public FileManifest preBuildManifest;
+        public FileManifest postBuildManifest;
+        public AssetBundleManifest manifest;
+
+        public virtual void BuildAll()
+        {
+            Caching.ClearCache();
+            string temp = Path.Combine(Constants.BUILD_ASSET_BUNDLE_PATH, Constants.MANIFEST_FILE);
+            if (File.Exists(temp)) Helper.CopyFile(temp, temp + ".last");
+            PrepareDirectory();
+            ProcessBuild();
+            GenerateManifest();
+            CaculateDiffer();
+        }
+
+        public virtual void PrepareDirectory()
+        {
+            if (Directory.Exists(Constants.BUILD_ASSET_BUNDLE_PATH) == false)
+            {
+                Directory.CreateDirectory(Constants.BUILD_ASSET_BUNDLE_PATH);
+            }
+        }
+
+        public virtual void ProcessBuild()
+        {
+            BuildPipeline.BuildAssetBundles(Constants.BUILD_ASSET_BUNDLE_PATH, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+        }
+
+        public virtual void GenerateManifest()
+        {
+            string abManifestFilePath = Constants.BUILD_ASSET_BUNDLE_PATH + Constants.ASSET_BUNDLE_MANIFEST_FILE;
+            string manifestFilePath = Constants.BUILD_ASSET_BUNDLE_PATH + Constants.ASSET_BUNDLE_MANIFEST_FILE + ".manifest";
+            string assetManifestFilePath = Constants.BUILD_ASSET_BUNDLE_PATH + Constants.MANIFEST_FILE;
+            if (File.Exists(assetManifestFilePath))
+            {
+                File.Delete(assetManifestFilePath);
+            }
+            if (File.Exists(abManifestFilePath) == false)
+            {
+                Helper.LogError("error caused by null ab manifest.");
+                return;
+            }
+            FileStream fs = new FileStream(assetManifestFilePath, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs);
+            // write ab manifest file;
+            string manifestMD5 = Helper.FileMD5(abManifestFilePath);
+            int manifestSize = Helper.FileSize(abManifestFilePath);
+            sw.WriteLine(Constants.ASSET_BUNDLE_MANIFEST_FILE + "|" + manifestMD5 + "|" + manifestSize);
+            string[] lines = File.ReadAllLines(manifestFilePath);
+            List<string> abs = new List<string>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                if (line.StartsWith("      Name: "))
+                {
+                    line = line.Replace("      Name: ", "");
+                    line = line.Trim();
+                    abs.Add(line);
+                }
+            }
+            AssetBundle bundle = AssetBundle.LoadFromFile(Constants.BUILD_ASSET_BUNDLE_PATH + "Assets");
+            manifest = bundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+            List<string> abs2 = new List<string>();
+            int count = 0;
+            while (abs.Count > 0)
+            {
+                for (int i = 0; i < abs.Count;)
+                {
+                    string ab = abs[i];
+                    string[] deps = manifest.GetAllDependencies(ab);
+                    if (deps.Length == count)
+                    {
+                        abs.RemoveAt(i);
+                        abs2.Add(ab);
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                count++;
+            }
+            for (int i = 0; i < abs2.Count; i++)
+            {
+                string ab = abs2[i];
+                string filePath = Constants.BUILD_ASSET_BUNDLE_PATH + ab;
+                int size = Helper.FileSize(filePath);
+                string md5 = Helper.FileMD5(filePath);
+                sw.WriteLine(ab + "|" + md5 + "|" + size);
+            }
+            sw.Close();
+            fs.Close();
+            bundle.Unload(true);
+        }
+
+        public virtual void CaculateDiffer()
+        {
+            preBuildManifest = new FileManifest(Constants.BUILD_ASSET_BUNDLE_PATH, Constants.MANIFEST_FILE + ".last");
+            preBuildManifest.Initialize();
+            Helper.DeleteFile(Path.Combine(Constants.BUILD_ASSET_BUNDLE_PATH, Constants.MANIFEST_FILE + ".last"));
+            postBuildManifest = new FileManifest(Constants.BUILD_ASSET_BUNDLE_PATH, Constants.MANIFEST_FILE);
+            postBuildManifest.Initialize();
+            FileManifest.DifferInfo differInfo = preBuildManifest.CompareWith(postBuildManifest);
+            string toast = string.Empty;
+            for (int i = 0; i < differInfo.Modified.Count; i++)
+            {
+                FileManifest.FileInfo fileInfo = differInfo.Modified[i];
+                Helper.Log("[{0}] has been modified.", fileInfo.Name);
+            }
+            if (differInfo.Modified.Count > 0)
+            {
+                toast += string.Format("{0} asset(s) has been modified.\n", differInfo.Modified.Count);
+            }
+            for (int i = 0; i < differInfo.Added.Count; i++)
+            {
+                FileManifest.FileInfo fileInfo = differInfo.Added[i];
+                Helper.Log("[{0}] has been added.", fileInfo.Name);
+            }
+            if (differInfo.Added.Count > 0)
+            {
+                toast += string.Format("{0} asset(s) has been added.\n", differInfo.Added.Count);
+            }
+            for (int i = 0; i < differInfo.Deleted.Count; i++)
+            {
+                FileManifest.FileInfo fileInfo = differInfo.Deleted[i];
+                string filePath1 = Constants.BUILD_ASSET_BUNDLE_PATH + fileInfo.Name;
+                string filePath2 = Constants.BUILD_ASSET_BUNDLE_PATH + fileInfo.Name + ".manifest";
+                Helper.Log("[{0}] has been deleted.", fileInfo.Name);
+                if (File.Exists(filePath1))
+                {
+                    File.Delete(filePath1);
+                }
+                if (File.Exists(filePath2))
+                {
+                    File.Delete(filePath2);
+                }
+            }
+            if (differInfo.Deleted.Count > 0)
+            {
+                toast += string.Format("{0} asset(s) has been deleted.\n", differInfo.Deleted.Count);
+            }
+            if (differInfo.HasDiffer == false)
+            {
+                Helper.Log("No asset to build.");
+                toast = "No asset to build.";
+            }
+            postBuildManifest.ToFile();
+            Helper.ShowToast(toast);
+        }
+    }
+}
---
+++ Editor/BuildTags.cs
@@ -0,0 +1,289 @@
+﻿//---------------------------------------------------------------------//
+//                    GNU GENERAL PUBLIC LICENSE                       //
+//                       Version 2, June 1991                          //
+//                                                                     //
+// Copyright (C) Wells Hsu, wellshsu@outlook.com, All rights reserved. //
+// Everyone is permitted to copy and distribute verbatim copies        //
+// of this license document, but changing it is not allowed.           //
+//                  SEE LICENSE.md FOR MORE DETAILS.                   //
+//---------------------------------------------------------------------//
+using EP.U3D.EDITOR.BASE;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace EP.U3D.EDITOR.ASSET
+{
+    public class BuildTags
+    {
+        public static Type WorkerType = typeof(BuildTags);
+
+        [MenuItem(Constants.ASSETS_SET_BUNDLE_TAGS)]
+        public static void Invoke1()
+        {
+            var worker = Activator.CreateInstance(WorkerType) as BuildTags;
+            worker.SetSelectTags();
+        }
+
+        [MenuItem(Constants.MENU_PATCH_BUILD_DEPS)]
+        public static void Invoke2()
+        {
+            var worker = Activator.CreateInstance(WorkerType) as BuildTags;
+            worker.GenAssetDeps();
+        }
+
+        [MenuItem(Constants.MENU_PATCH_BUILD_TAG)]
+        public static void Invoke3()
+        {
+            var worker = Activator.CreateInstance(WorkerType) as BuildTags;
+            worker.RebuileTags();
+        }
+
+        public List<string> doneAssets = new List<string>();
+        public Dictionary<string, string> scensitiveChar = new Dictionary<string, string>() {
+            { "_", "_"},
+            { " ", ""},
+            { "#", ""},
+            { "[", ""},
+            { "]", ""}
+        };
+
+        public virtual void RebuileTags()
+        {
+            doneAssets.Clear();
+            SetAllTags();
+            ClearAllTags();
+            string toast = "Build assets tag done.";
+            Helper.Log(toast);
+            Helper.ShowToast(toast);
+        }
+
+        public virtual void ClearAllTags()
+        {
+            List<string> assets = new List<string>();
+            Helper.CollectAssets(Application.dataPath, assets, ".cs", ".js", ".h", ".lua", ".dll", ".a", ".lib", ".meta", ".tpsheet", ".DS_Store");
+            for (int i = 0; i < assets.Count; i++)
+            {
+                string asset = assets[i];
+                if (doneAssets.Contains(asset) == false)
+                {
+                    AssetImporter assetImporter = AssetImporter.GetAtPath(asset);
+                    if (assetImporter && string.IsNullOrEmpty(assetImporter.assetBundleName) == false)
+                    {
+                        assetImporter.SetAssetBundleNameAndVariant(string.Empty, string.Empty);
+                        Helper.Log("clear asset bundle tag of {0}", asset);
+                    }
+                }
+            }
+            AssetDatabase.Refresh();
+        }
+
+        public virtual void SetAllTags()
+        {
+            const string assetsDir = "Assets/";
+            List<string> sourceAssets = new List<string>();
+            Helper.CollectAssets(Constants.BUNDLE_ASSET_WORKSPACE, sourceAssets, ".cs", ".js", ".meta", ".tpsheet", ".DS_Store");
+            Helper.CollectAssets(Constants.RESOURCE_WORKSPACE + "/Bundle", sourceAssets, ".cs", ".js", ".meta", ".tpsheet", ".DS_Store");
+            for (int i = 0; i < sourceAssets.Count; i++)
+            {
+                string asset = sourceAssets[i];
+                string path = asset;
+                doneAssets.Add(asset);
+                string temp = asset.Substring(0, asset.LastIndexOf("/"));
+                foreach (var item in scensitiveChar)
+                {
+                    if (temp.Contains(item.Key)) Helper.LogWarning("invalid char '{0}' in asset path: {1}", item.Key, asset);
+                }
+                AssetImporter assetImporter = AssetImporter.GetAtPath(asset);
+                asset = asset.Substring(assetsDir.Length);
+                asset = asset.Substring(0, asset.LastIndexOf("/"));
+                asset = asset.Replace("\\", "/").Replace("/", "_");
+                asset += Constants.ASSET_BUNDLE_FILE_EXTENSION;
+                asset = asset.ToLower();
+                if (assetImporter)
+                {
+                    if (assetImporter.assetBundleName != asset) assetImporter.SetAssetBundleNameAndVariant(asset, string.Empty);
+                    if (path.EndsWith(".jpg") || path.EndsWith(".jpeg") || path.EndsWith(".png")) FormatTextureSize(path);
+                }
+            }
+            AssetDatabase.Refresh();
+
+            Dictionary<string, List<string>> dependAssets = Helper.CollectAssetDependency(sourceAssets);
+            Dictionary<string, List<string>>.Enumerator ir = dependAssets.GetEnumerator();
+            for (int i = 0; i < dependAssets.Count; i++)
+            {
+                ir.MoveNext();
+                KeyValuePair<string, List<string>> kvp = ir.Current;
+                List<string> assets = kvp.Value;
+                for (int j = 0; j < assets.Count; j++)
+                {
+                    string asset = assets[j];
+                    string path = asset;
+                    if (doneAssets.Contains(asset) == false)
+                    {
+                        doneAssets.Add(asset);
+                        if (asset.Contains("Editor/"))
+                        {
+                            Helper.LogWarning("ignore editor asset deps: {0}", asset);
+                        }
+                        else
+                        {
+                            string temp = asset.Substring(0, asset.LastIndexOf("/"));
+                            foreach (var item in scensitiveChar)
+                            {
+                                if (temp.Contains(item.Key)) Helper.LogWarning("invalid char '{0}' in asset path: {1}", item.Key, asset);
+                            }
+                            AssetImporter assetImporter = AssetImporter.GetAtPath(asset);
+                            asset = asset.Substring(assetsDir.Length);
+                            asset = asset.Substring(0, asset.LastIndexOf("/"));
+                            asset = asset.Replace("\\", "/").Replace("/", "_");
+                            foreach (var item in scensitiveChar)
+                            {
+                                asset = asset.Replace(item.Key, item.Value);
+                            }
+                            asset += Constants.ASSET_BUNDLE_FILE_EXTENSION;
+                            asset = asset.ToLower();
+                            if (assetImporter)
+                            {
+                                if (assetImporter.assetBundleName != asset) assetImporter.SetAssetBundleNameAndVariant(asset, string.Empty);
+                                if (path.EndsWith(".jpg") || path.EndsWith(".jpeg") || path.EndsWith(".png")) FormatTextureSize(path);
+                            }
+                        }
+                    }
+                }
+            }
+            AssetDatabase.Refresh();
+        }
+
+        public virtual void FormatTextureSize(string asset)
+        {
+            const string maxTextureSize = "maxTextureSize:";
+            Texture texture = AssetDatabase.LoadAssetAtPath<Texture>(asset);
+            if (texture)
+            {
+                string meta = Helper.StringFormat("{0}.meta", asset);
+                if (File.Exists(meta))
+                {
+                    bool isModified = false;
+                    string[] lines = File.ReadAllLines(meta);
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string line = lines[i];
+                        if (string.IsNullOrEmpty(line) == false)
+                        {
+                            if (line.Contains(maxTextureSize))
+                            {
+                                int max = CalcTextureSize(texture);
+                                if (texture.width != max || texture.height != max)
+                                {
+                                    isModified = true;
+                                    line = Helper.StringFormat("{0}{1} {2}", line.Substring(0, line.IndexOf(maxTextureSize)), maxTextureSize, max);
+                                    lines[i] = line;
+                                }
+                            }
+                        }
+                    }
+                    if (isModified)
+                    {
+                        File.Delete(meta);
+                        File.WriteAllLines(meta, lines);
+                    }
+                }
+            }
+        }
+
+        public virtual int CalcTextureSize(Texture texture)
+        {
+            if (texture)
+            {
+                int max = Mathf.Max(texture.width, texture.height);
+                if (max <= 32)
+                {
+                    return 64;
+                }
+                else if (max <= 64)
+                {
+                    return 128;
+                }
+                else if (max <= 128)
+                {
+                    return 256;
+                }
+                else if (max <= 256)
+                {
+                    return 512;
+                }
+                else if (max <= 512)
+                {
+                    return 1024;
+                }
+                else if (max <= 1024)
+                {
+                    return 2048;
+                }
+                else if (max <= 2048)
+                {
+                    return 4096;
+                }
+                else
+                {
+                    return 8192;
+                }
+            }
+            return 8192;
+        }
+
+        public virtual void GenAssetDeps()
+        {
+            List<string> sourceAssets = new List<string>();
+            Helper.CollectAssets(Constants.BUNDLE_ASSET_WORKSPACE, sourceAssets, ".cs", ".js", ".meta", ".tpsheet", ".DS_Store");
+            Helper.CollectAssets(Constants.RESOURCE_WORKSPACE + "/Bundle", sourceAssets, ".cs", ".js", ".meta", ".tpsheet", ".DS_Store");
+            Dictionary<string, List<string>> dependAssets = Helper.CollectAssetDependency(sourceAssets);
+            Dictionary<string, List<string>>.Enumerator ir = dependAssets.GetEnumerator();
+            string destPath = Constants.PROJ_PATH + "Library/AssetDeps.txt";
+            if (Helper.HasFile(destPath)) Helper.DeleteFile(destPath);
+            using (var destFile = File.Open(destPath, FileMode.Create))
+            {
+                StreamWriter sw = new StreamWriter(destFile);
+                for (int i = 0; i < dependAssets.Count; i++)
+                {
+                    ir.MoveNext();
+                    KeyValuePair<string, List<string>> kvp = ir.Current;
+                    sw.WriteLine(kvp.Key);
+                    for (int j = 0; j < kvp.Value.Count; j++)
+                    {
+                        string asset = kvp.Value[j];
+                        if (asset.Contains("Editor/") == false && asset != kvp.Key) sw.WriteLine("\t" + asset);
+                    }
+                }
+            }
+            string toast = "Build assets deps done.";
+            Helper.Log("[FILE@{0}] {1}", destPath, toast);
+        }
+
+        public virtual void SetSelectTags()
+        {
+            UnityEngine.Object[] selected = Selection.objects;
+            for (int i = 0; i < selected.Length; i++)
+            {
+                UnityEngine.Object asset = selected[i];
+                if (asset)
+                {
+                    string assetPath = AssetDatabase.GetAssetPath(asset);
+                    string bundleName = assetPath.Substring("Assets/".Length);
+                    bundleName = bundleName.Substring(0, bundleName.LastIndexOf("/"));
+                    bundleName = bundleName.Replace("/", "_");
+                    bundleName = bundleName.ToLower();
+                    bundleName = bundleName + Constants.ASSET_BUNDLE_FILE_EXTENSION;
+                    AssetImporter assetImporter = AssetImporter.GetAtPath(assetPath);
+                    if (assetImporter)
+                    {
+                        assetImporter.SetAssetBundleNameAndVariant(bundleName, string.Empty);
+                    }
+                }
+            }
+        }
+    }
+}
